832858db9785147e154a7cdcd5b85efa38f13770
FJoemarie <60754429+FJoemarie@users.noreply.github.com>
2021-02-14 19:11:04 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 let btn = document.querySelectorAll('.drum');
 let audio;
 for (i = 0; i < btn.length; i++) {
-  btn[i].addEventListener('click', () => {
+  btn[i].addEventListener('click', function(){
     let btnInnerHtml = this.innerHTML;
     keyClick(btnInnerHtml);
     btnAnimate(btnInnerHtml);
